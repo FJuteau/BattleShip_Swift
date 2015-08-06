78f3d889c24f155a7c17977f84dba3b474551b8b
Juteau François <f.juteau@gmail.com>
2015-08-06 07:13:34 -0700

    Initial Commit

---
+++ BattleShip_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BattleShip_Swift
+//
+//  Created by François Juteau on 06/08/2015.
+//  Copyright (c) 2015 François Juteau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
